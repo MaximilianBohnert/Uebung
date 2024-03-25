252a93d6be4f151a94ac79520909741689761b9d
MaximilianBohnert <maxi@diebohnerts.de>
2024-03-25 19:19:11 +0100

    Entfernungsabhägige LED intensität und LED Anzahl

---
+++ Distance_LED_Range.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import time
+import VL53L0X
+import spidev
+
+
+def writeWS2812(spi, color):
+        list=[]
+        for rgb in color:
+                for byte in [rgb[1],rgb[0],rgb[2]]:
+                        for ibit in range(7,-1,-1):
+                                if(byte>>ibit)&1 == 1:
+                                        list.append(0xF8)
+                                else:
+                                        list.append(0x80)
+        spi.xfer(list, int(8/1.25e-6))
+
+
+# Create a VL53L0X object
+tof = VL53L0X.VL53L0X()
+
+# Start ranging
+tof.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+
+timing = tof.get_timing()
+if (timing < 20000):
+    timing = 20000
+print ("Timing %d ms" % (timing/1000))
+
+while (1):
+    distance = tof.get_distance()
+    if (distance > 0):
+        print ("%d mm, %d cm" % (distance, (distance/10)))
+
+    spi = spidev.SpiDev()
+    spi.open(0,0)
+
+    if (distance < 150):
+        writeWS2812(spi, [[30,0,0]]*5)
+    if (distance < 250):
+        writeWS2812(spi, [[25,0,0],[25,0,0],[25,0,0],[25,0,0],[0,0,0]])
+    if (distance < 400):
+        writeWS2812(spi, [[20,0,0],[20,0,0],[20,0,0],[0,0,0],[0,0,0]])
+    if (distance < 550):
+        writeWS2812(spi, [[15,0,0],[15,0,0],[0,0,0],[0,0,0],[0,0,0]])
+    else:
+        writeWS2812(spi, [[10,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]])
+
+
+    time.sleep(0.2)
+
+tof.stop_ranging()
+
+#spi = spidev.SpiDev()
+#spi.open(0,0)
+#if (distance < 100):
+#	writeWS2812(spi, [[10,0,0]]*5)
+#if (distance < 250):
+#	writeWS2812(spi, [[10,10,0]]*5)
+#else:
+#	writeWS2812(spi, [[0,0,10]]*5)
